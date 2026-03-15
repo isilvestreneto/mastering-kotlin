a03ed27f35fa3408fd3e235305e81ae5dcf8ee7b
Ivan <isilvestreneto@gmail.com>
2026-03-15 19:48:03 -0300

    feat atividade 1

--- app/atividades/src/main/java/proway/capgemini/atividades/Atividade1.kt
+++ app/atividades/src/main/java/proway/capgemini/atividades/Atividade1.kt
@@ -1,14 +1,19 @@
 package proway.capgemini.atividades
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 import java.util.Locale
 import java.util.Scanner
 
 val VALORES_PERMITIDOS = arrayOf("A", "B", "C", "D", "E", "F");
 
+val TIPOS_ALTERNATIVAS = arrayOf("A", "B", "C", "D", "E");
+val TIPOS_ALTERNATIVAS_ATUALIZACAO = arrayOf("A", "B", "C", "D");
+
 val leitura = Scanner(System.`in`);
 
-val pets: Array<Pet> =
+val pets: MutableList<Pet> = mutableListOf();
 
 fun main() {
     printInitialLogo();
@@ -17,8 +22,7 @@ fun main() {
 }
 
 private fun runMenu() {
-    val entrada = catchMenu();
-
+    var entrada = catchMenu();
     val entradaUppercase = entrada.uppercase(Locale.ROOT);
 
     if (!VALORES_PERMITIDOS.contains(entradaUppercase)) {
@@ -33,6 +37,7 @@ private fun runMenu() {
                 "D" -> alterar();
                 "E" -> remover();
             }
+            entrada = catchMenu();
         }
         finalizar();
     }
@@ -45,61 +50,177 @@ private fun catchMenu(): String {
 }
 
 fun remover() {
-    TODO("Not yet implemented")
+    if (pets.isEmpty()) {
+        println("Nenhum pet cadastrado.");
+        return;
+    }
+
+    println("Informe o nome do pet que deseja remover:")
+    val nome = leitura.nextLine();
+    val petEncontrado = pets.find { it.nome.equals(nome, ignoreCase = true); }
+
+    if (petEncontrado == null) {
+        println("Nenhum pet encontrado...");
+        return;
+    }
+    pets.remove(petEncontrado);
+    println("Pet removido com sucesso!");
 }
 
 fun alterar() {
-    TODO("Not yet implemented")
+    println("Iniciando alteração de pet...")
+    if (pets.isEmpty()) {
+        println("Nenhum pet cadastrado.");
+        return;
+    }
+
+    println("Informe o nome do pet que deseja alterar:")
+    val nome = leitura.nextLine();
+    val petEncontrado = pets.find { it.nome.equals(nome, ignoreCase = true) }
+
+    if (petEncontrado == null) {
+        println("Nenhum pet encontrado...");
+        return;
+    }
+
+    println("Pet do tipo ${petEncontrado.tipo} encontrado: ${petEncontrado.nome} - Nascido em ${petEncontrado.dataNascimento}")
+    println("Informe os novos dados do pet:")
+    println("Nome:")
+    val novoNome = validarNome(leitura);
+    println("Data de nascimento: (formato DD/MM/AAAA | Exemplo: \"30/09/1995\")")
+    val novaDataNascimento = parseLocalDate(leitura);
+    println("Tipo: (digite a letra correspondente)")
+    var novoTipo: String;
+    do {
+        println("A) Canino \nB) Felino \nC) Roedor \nD) Ave")
+
+        novoTipo = leitura.nextLine();
+
+        if (!TIPOS_ALTERNATIVAS_ATUALIZACAO.contains(novoTipo.uppercase(Locale.ROOT))) {
+            println("Opção inválida, tente novamente.");
+        }
+    } while (!TIPOS_ALTERNATIVAS_ATUALIZACAO.contains(novoTipo.uppercase(Locale.ROOT)));
+
+    val petAlterado: Pet = when (novoTipo.uppercase(Locale.ROOT)) {
+        "A" -> Canino(novoNome, novaDataNascimento);
+        "B" -> Felino(novoNome, novaDataNascimento);
+        "C" -> Roedor(novoNome, novaDataNascimento);
+        "D" -> Ave(novoNome, novaDataNascimento);
+        else -> {
+            throw IllegalArgumentException("Tipo inválido")
+        }
+    }
+
+    // Substitui o pet antigo pelo novo
+    val index = pets.indexOf(petEncontrado)
+    pets[index] = petAlterado;
+
+    println("Pet alterado com sucesso!");
+}
+
+fun validarNome(scanner: Scanner): String {
+    var nome: String;
+
+    do {
+        nome = scanner.nextLine();
+        if (nome.isBlank()) {
+            println("Nome não pode ser vazio, tente novamente.");
+        }
+    } while (nome.isBlank());
+
+    return nome;
+}
+
+fun parseLocalDate(scanner: Scanner): LocalDate {
+    var data: LocalDate? = null;
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    do {
+        val input = scanner.nextLine();
+        try {
+            data = LocalDate.parse(input, formatter);
+        } catch (e: DateTimeParseException) {
+            println("Data inválida, tente novamente. Formato esperado: DD/MM/AAAA");
+        }
+    } while (data == null);
+
+    return data;
 }
 
 fun pesquisar() {
-    TODO("Not yet implemented")
+    println("Pesquisando pet...")
+
+    if (pets.isEmpty()) {
+        println("Nenhum pet cadastrado.");
+        return;
+    }
+
+    println("Informe o nome do pet que deseja pesquisar:")
+    val nome = leitura.nextLine();
+
+    val petEncontrado = pets.find { it.nome.equals(nome, ignoreCase = true) }
+
+    petEncontrado?.let {
+        println("Pet do tipo ${it.tipo} encontrado: ${it.nome} - Nascido em ${it.dataNascimento}")
+    } ?: println("Nenhum pet encontrado...")
+
 }
 
 fun listar() {
-    TODO("Not yet implemented")
+    println("Listando pets cadastrados...")
+
+    if (pets.isEmpty()) {
+        println("Nenhum pet cadastrado.");
+    } else {
+        pets.forEachIndexed { index, pet ->
+            println("${index + 1} - ${pet.nome} (${pet.tipo}) - Nascido em ${pet.dataNascimento}")
+        }
+    }
 }
 
 fun cadastrar() {
     println("Iniciando cadastro de pet...")
     println("Informe os dados da pet:")
     println("Nome:")
-    val nome = leitura.nextLine();
+    val nome = validarNome(leitura);
     println("Data de nascimento: (formato DD/MM/AAAA | Exemplo: \"30/09/1995\")")
-    val dataNascimento = leitura.nextLine();
+    val dataNascimento = parseLocalDate(leitura);
 
     println("Tipo: (digite a letra correspondente)")
-    // Aqui vai questões A B C D para tipos: canino, felino, roedor, ave, etc.
-    println("A) Canino \n B) Felino \n C) Roedor \n D) Felino")
-    val raca = leitura.nextLine();
 
-    when (raca) {
-        "A" -> {
-            // Montar objeto canino
-            val pet = Canino(nome, dataNascimento);
-        };
-        "B" -> {
-            val pet = Felino(nome, dataNascimento);
-        };
-        "C" -> {
-            val pet = Roedor(nome, dataNascimento);
-        };
-        "D" -> {
-            val pet = Ave(nome, dataNascimento);
-        };
-        else -> {
+
+    var tipo: String;
+
+    do {
+        println("A) Canino \nB) Felino \nC) Roedor \nD) Ave \nE) Retornar ao menu")
+
+        tipo = leitura.nextLine();
+
+        if (!TIPOS_ALTERNATIVAS.contains(tipo.uppercase(Locale.ROOT))) {
             println("Opção inválida, tente novamente.");
-            cadastrar();
+        }
+    } while (!TIPOS_ALTERNATIVAS.contains(tipo.uppercase(Locale.ROOT)));
+
+    if (tipo.uppercase(Locale.ROOT) == "E") {
+        return;
+    }
+
+    val pet: Pet = when (tipo) {
+        "A" -> Canino(nome, dataNascimento);
+        "B" -> Felino(nome, dataNascimento);
+        "C" -> Roedor(nome, dataNascimento);
+        "D" -> Ave(nome, dataNascimento);
+        else -> {
+            throw IllegalArgumentException("Tipo inválido")
         }
     }
 
     persistirDado(pet);
-
-    println("Pet cadastrado com sucesso!");
 }
 
 fun persistirDado(pet: Pet) {
-
+    pets.add(pet);
+    println("Pet cadastrado com sucesso!");
 }
 
 fun finalizar() {
@@ -117,7 +238,7 @@ fun printMenu() {
     println("F - Finalizar")
     println("")
 
-    print("Entre com uma opção:")
+    print("Entre com uma opcão:")
 }
 
 private fun printInitialLogo() {
@@ -137,28 +258,28 @@ private fun printInitialLogo() {
 
 interface Pet {
     val nome: String
-    val dataNascimento: String
+    val dataNascimento: LocalDate
     val tipo: Tipo
 }
 
 // Usando data class para facilitar comparação e log (toString)
-data class Canino(override val nome: String, override val dataNascimento: String) : Pet {
+data class Canino(override val nome: String, override val dataNascimento: LocalDate) : Pet {
     override val tipo = Tipo.CANINO
 }
 
-data class Felino(override val nome: String, override val dataNascimento: String) : Pet {
+data class Felino(override val nome: String, override val dataNascimento: LocalDate) : Pet {
     override val tipo = Tipo.FELINO
 }
 
-data class Roedor(override val nome: String, override val dataNascimento: String) : Pet {
+data class Roedor(override val nome: String, override val dataNascimento: LocalDate) : Pet {
     override val tipo = Tipo.ROEDOR
 }
 
-data class Ave(override val nome: String, override val dataNascimento: String) : Pet {
+data class Ave(override val nome: String, override val dataNascimento: LocalDate) : Pet {
     override val tipo = Tipo.AVE
 }
 
-enum Tipo {
+enum class Tipo {
     CANINO, FELINO, ROEDOR, AVE
 }
 
